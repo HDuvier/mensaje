4b8bdf1355424e9fabcd6fecc4319462cd89dfba
HDuvier <hectord_castro@hotmail.com>
2023-07-02 23:46:08 -0500

    se completa el crud basico

--- src/MensajeDAO.java
+++ src/MensajeDAO.java
@@ -1,6 +1,8 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class MensajeDAO {
 
@@ -10,7 +12,7 @@ public class MensajeDAO {
         try(Connection connexion = dbConnect.get_connetion()){
             PreparedStatement ps = null;
             try {
-                String consulta = "insert into mensajes (mensaje,autor_mensaje) values (?,?)";
+                String consulta = "insert into mensajes (mensaje,autor_mensaje,fecha_mensaje) values (?,?,current_timestamp())";
                 ps = connexion.prepareStatement(consulta);
                 ps.setString(1, mensaje.getMensaje());
                 ps.setString(2, mensaje.getAutorMensaje());
@@ -25,13 +27,72 @@ public class MensajeDAO {
         }
 
     }
-    public static void leerMensajesDB(){
+    public static ArrayList leerMensajesDB(){
+        conexion dbConnect = new conexion();
+        ArrayList arrayMensajes = new ArrayList<>();
 
+        try (Connection connexion = dbConnect.get_connetion()){
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+
+            String consulta = "select * from mensajes";
+            ps = connexion.prepareStatement(consulta);
+            rs  = ps.executeQuery();
+            while (rs.next()){
+                //ArrayList mensajes = new ArrayList<>();
+                arrayMensajes.add(rs.getInt("id_mensaje"));
+                arrayMensajes.add(rs.getString("mensaje"));
+                arrayMensajes.add(rs.getString("autor_mensaje"));
+                arrayMensajes.add(rs.getString("fecha_mensaje"));
+                arrayMensajes.add("\n");
+            };
+
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return arrayMensajes;
     }
     public static void borrarMensajeDB(int idMensaje){
+        conexion dbConnexion = new conexion();
+         try (Connection connexion = dbConnexion.get_connetion()) {
+             PreparedStatement ps = null;
+             try {
+                 String consulta = "delete from mensajes where id_mensaje = ?";
+                 ps = connexion.prepareStatement(consulta);
+                 ps.setInt(1,idMensaje);
+                 ps.executeUpdate();
+                 System.out.println("fue borrado");
+             } catch (SQLException ex) {
+                 System.out.println(ex);
+                 throw new RuntimeException(ex);
+             }
+         } catch (SQLException e) {
+             System.out.println(e);
+             throw new RuntimeException(e);
+         }
 
     }
     public static void actualizarMensajeDB(Mensaje mensaje){
+        conexion dbConexion = new conexion();
+        try (Connection connection = dbConexion.get_connetion()){
+            PreparedStatement ps = null;
+            try {
+                String consulta = "update mensajes set mensaje = ? where id_mensaje = ?";
+                ps = connection.prepareStatement(consulta);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setInt(2,mensaje.getIdMensaje());
+                ps.executeUpdate();
+                System.out.println("actualizado");
+
+            } catch (Exception ex) {
+                System.out.println("no se actualizo");
+                throw new RuntimeException(ex);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+            throw new RuntimeException(e);
+        }
 
     }
 }
--- src/MensajeService.java
+++ src/MensajeService.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MensajeService {
@@ -19,12 +20,32 @@ public class MensajeService {
         MensajeDAO.crearMensajeDB(registro);
     }
     public static void listarMensaje(){
+        ArrayList leerMensajes = MensajeDAO.leerMensajesDB();
+        for (int i = 0; i < leerMensajes.size(); i++) {
+            System.out.println(leerMensajes.get(i));
+        }
+
 
     }
     public static void borrarMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Id del mensaje a borrar ");
+        int id = sc.nextInt();
+
+        MensajeDAO.borrarMensajeDB(id);
 
     }
     public static void editarMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingresa mensaje nuevo");
+        String mensaj = sc.nextLine();
+        System.out.println("ingresa el id del mensaje");
+        int idMens = sc.nextInt();
+
+        Mensaje editado = new Mensaje();
+        editado.setMensaje(mensaj);
+        editado.setIdMensaje(idMens);
+        MensajeDAO.actualizarMensajeDB(editado);
 
     }
 }
